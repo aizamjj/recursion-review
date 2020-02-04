94998319ecb2f2cde9fdd199dcbc5fb828fd1154
aizamjj <aizamjig@gmail.com>
2020-02-04 10:24:35 -0800

    Passes specs utilizing recursion

--- src/getElementsByClassName.js
+++ src/getElementsByClassName.js
@@ -4,7 +4,23 @@
 // };
 
 // But instead we're going to implement it from scratch:
-var getElementsByClassName = function(className
-) {
+var getElementsByClassName = function(className, node) {
+  var result = [];
+  node = node || document.body;
+
+  if (node.classList) {
+    if (node.classList.contains(className)) {
+      result.push(node);
+    }
+  }
+
+  if (node.childNodes) {
+    node.childNodes.forEach(function(element) {
+      var childResult = getElementsByClassName(className, element);
+      result = result.concat(childResult);
+    });
+  }
+
+  return result;
   // your code here
 };
